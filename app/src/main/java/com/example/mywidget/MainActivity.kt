c2e2110bb98decbc69fc8d0739cb9f542f6d5df4
Xalam <ganufvyandana@gmail.com>
2020-09-15 22:52:30 +0700

    Notification

--- app/src/main/java/com/example/mywidget/MainActivity.kt
+++ app/src/main/java/com/example/mywidget/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.mywidget
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,27 +34,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btn_start.setOnClickListener(this)
         btn_stop.setOnClickListener(this)
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_assistant)
-            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_assistant))
-            .setContentTitle(resources.getString(R.string.content_title))
-            .setContentText(resources.getString(R.string.content_text))
-            .setSubText(resources.getString(R.string.subtext))
-            .setAutoCancel(true)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
-            channel.description = CHANNEL_NAME as String?
-            builder.setChannelId(CHANNEL_ID)
-
-            notificationManager.createNotificationChannel(channel)
-        }
-        val notification = builder.build()
-
-        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -84,5 +66,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val tm = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         tm.cancel(JOB_ID)
         Toast.makeText(this, "Job Service canceled", Toast.LENGTH_SHORT).show()
+    }
+
+    fun sendNotif(view: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com")) //Can Also Intent Activity
+        val pendingIntent = PendingIntent.getActivity(this,0, intent,0)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_assistant)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_assistant))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_NAME as String?
+            builder.setChannelId(CHANNEL_ID)
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = builder.build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
     }
 }
